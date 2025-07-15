9c7a016f1a58c04340b4c3bd8ec5eac5d6e15795
Shreya Patil <144721490+shreya227108@users.noreply.github.com>
2025-07-15 08:12:11 +0530

    Add files via upload

---
+++ Day_01/Day_1.js
@@ -0,0 +1,51 @@
+console.log("Hii everyone");
+console.log('This is shreya here!')
+console.log("Welcome to my JavaScript Series!");
+
+//Variables
+fullname="Tony Stark";
+age=24;
+price=98.6;
+x=null;
+y=undefined;
+isFollow= false;//Boolean= true or false
+
+console.log(fullname);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isFollow);
+
+//Var
+var age=30;
+var age= 50;
+console.log(age);
+
+//Let
+let myname="Shreya";
+let a;
+console.log(myname);
+console.log(a);
+/*In let you can leave a variable without assigning any value to it
+its default type becomes undefined*/
+
+//Const
+const pie= 3.14;
+console.log(pie);
+
+//Datatypes/non-premitive/objects
+const student = {
+    name : "Rahul",
+    age : 20,
+    cgpa : 9.4,
+    ispass : true
+};
+
+//Updating the values in objects
+student["age"] = student["age"] + 1;
+student["name"] = "shreya Patil";
+
+console.log(student.age);
+console.log(student["name"]);
+
