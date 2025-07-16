4f5c23c33101706e422904ad5461e6cb36b3fda1
Shreya Patil <144721490+shreya227108@users.noreply.github.com>
2025-07-16 23:15:02 +0530

    Add files via upload

---
+++ Practice_Set_02/script.js
@@ -0,0 +1,25 @@
+//alert("Helloo");
+
+/*Q1
+let num = prompt("Enter a number:");
+if(num % 5 == 0){
+    console.log(num, " is a multiple of 5");
+}else{
+    console.log(num, " is not a multiple of 5")
+}
+*/
+
+//Q2
+let marks = prompt("Enter your marks:");
+
+if( marks >= 80 && marks <= 100){
+    console.log("A");
+}else if(marks >= 70 && marks <= 89){
+    console.log("B");
+}else if(marks >= 60 && marks <= 69){
+    console.log("C");
+}else if(marks >= 50 && marks <= 59){
+    console.log("D");
+}else if(marks >= 0 && marks <= 49){
+    console.log("F");
+}
