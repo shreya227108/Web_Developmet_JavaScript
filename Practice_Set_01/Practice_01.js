76c12df0f3d082ecf9123c0869b757efd9b73e84
Shreya Patil <144721490+shreya227108@users.noreply.github.com>
2025-07-16 08:12:05 +0530

    Add files via upload

---
+++ Practice_Set_01/Practice_01.js
@@ -0,0 +1,20 @@
+const product = {
+    title: "Pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+}
+
+console.log(product);
+
+const profile = {
+    userName: 'shradhakhapra',
+    isfollow: false,
+    posts: 195,
+    followers: "569k",
+    following: 4,
+    Description:  "Apna college | Ex-Microsoft, DRDO to educate someone"
+}
+
+console.log(profile);
+console.log(typeof["username"]);
